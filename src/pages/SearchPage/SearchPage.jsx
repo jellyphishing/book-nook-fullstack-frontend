948030924f04fbdb700777292c9d41aa85847b9a
Lorraine Martin <lolocodes2001@gmail.com>
2023-10-20 08:55:40 -0700

    added new component SearchBar and import statements to SearchPage

--- src/pages/SearchPage/SearchPage.jsx
+++ src/pages/SearchPage/SearchPage.jsx
@@ -1,5 +1,9 @@
-import { link } from "react-router-dom";
 import axios from "axios";
+import React from "react";
+import SearchBar from "../../components/SearchBar/SearchBar";
+import { useState } from "react";
+import ResultsList from "../../components/ResultsList/ResultsList";
+import "./SearchPage.css";
 
 const SearchPage = () => {
   return <h1>Search Page</h1>;
