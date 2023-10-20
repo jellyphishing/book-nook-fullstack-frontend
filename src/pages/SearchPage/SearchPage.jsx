a533e89716018bc18c3dc83984611f85b4611578
Lorraine Martin <lolocodes2001@gmail.com>
2023-10-20 09:27:42 -0700

    added logic for SearchBar jsx and css

--- src/pages/SearchPage/SearchPage.jsx
+++ src/pages/SearchPage/SearchPage.jsx
@@ -6,35 +6,37 @@ import ResultsList from "../../components/ResultsList/ResultsList";
 import "./SearchPage.css";
 
 const SearchPage = () => {
-    const [searchTerm, setSearchTerm] = useState("");
-    const [searchResults, setSearchResults] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
 
-    const fetchBooks = async () => {
-        try{
-            let lowerCaseSearch = searchTerm.toLowerCase();
-            let response = await axios.get(
-                `http`
-            )
-        }
+  const fetchBooks = async () => {
+    try {
+      let lowerCaseSearch = searchTerm.toLowerCase();
+      let response = await axios.get(
+        `https://localhost:5001/api/bookId/${lowerCaseSearch}/`
+      );
+      setSearchResults(response.data.bookList);
+    } catch (error) {
+      console.log("Error in fetchBooks: ", error);
     }
+  };
 
+  const handleSUbmit = (e) => {
+    e.preventDefault();
+    fetchBooks();
+  };
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    return <h1>Search Page</h1>;
+  return (
+    <div classNAme="container search">
+      <h1>Search Page!</h1>
+      <SearchBar
+        searchTerm={searchTerm}
+        setSearchTerm={setSearchTerm}
+        handleSubmit={handleSUbmit}
+      />
+      <ResultsList searchResults={searchResults} />
+    </div>
+  );
 };
 
 export default SearchPage;
